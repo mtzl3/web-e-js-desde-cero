a966193bd327edd78ec334aea2094af0833ddd0c
mtzl3 <lopezmetz621@gmail.com>
2025-02-09 22:51:25 -0600

    Tipos de datos

---
+++ 1.fundamentos-js/tipos-de-dato.js
@@ -0,0 +1,27 @@
+console.log("Primer clase - Ejemplo")
+
+console.log( typeof "redonda" )
+
+console.log( 'hola mundo' )
+
+console.log( "El tipo de dato false es:", typeof false )
+
+console.log("----------------Tipos de datos ej JavaScript-----------------")
+console.log("42 es un tipo de dato: " + typeof 42)
+console.log("Veinticico es un tipo de dato: " + typeof veinticinco)
+console.log("-666 es un tipo de dato: " + typeof -666)
+console.log("true es un tipo de dato: " + typeof true)
+console.log("0 es un tipo de dato: " + typeof 0)
+console.log("'' es un tipo de dato: "+ typeof '')
+console.log("null es un tipo de dato: "+ typeof null)
+console.log("undefinied es un tipo de dato: "+ typeof undefined)
+console.log("FALSE es un tipo de dato: "+ typeof FALSE)
+
+
+console.log("---------------------Extras----------------------------------")
+console.log("[1, 2, 3] es un tipo de dato: " + typeof [1, 2, 3]); // Objeto
+console.log("{ nombre: 'Juan' } es un tipo de dato: " + typeof { nombre: "Juan" }); // Objeto
+console.log("function(){} es un tipo de dato: " + typeof function(){}); // Función
+console.log("Symbol('id') es un tipo de dato: " + typeof Symbol("id")); // Simbolo
+console.log("BigInt(9007199254740991) es un tipo de dato: " + typeof BigInt(2338288457861884)); // Bingint
+
